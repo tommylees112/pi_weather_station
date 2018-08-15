bb2fb556b6928df2d6c56c68554f71220a2975a7
Tommy Lees <thomas.lees112@gmail.com>
2018-08-15 15:30:35 +0100

    () issue fixed

--- weatherstation2.py
+++ weatherstation2.py
@@ -104,6 +104,6 @@ rain_sensor.when_activated = rain
 
 #perpetually prints the wind speed sleeping for every WIND_SLEEP_TIME seconds
 while True:
-  print("The wind speed is " + str(round(wind(WIND_SLEEP_TIME),2) + "kph \n"))
+  print("The wind speed is " + str(round(wind(WIND_SLEEP_TIME),2)) + "kph \n")
   print("The cumulative rainfall is currently" + str(rainfall) + " mm\n")
   sleep(WIND_SLEEP_TIME)
